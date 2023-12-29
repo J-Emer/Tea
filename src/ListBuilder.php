7024224bf303c4604cb7b5263f2e8f071e6c63b6
PartumGames <jemerson6989@gmail.com>
2023-12-28 18:49:36 -0600

    Update ListBuilder.php
    removed the BuildCategoriesList() from ListBuilder -> users can now use the $categories property

--- src/ListBuilder.php
+++ src/ListBuilder.php
@@ -17,14 +17,6 @@ class ListBuilder
     }
 
     /**
-     * Builds a list of categories
-     */
-    public function BuildCategoriesList()
-    {
-
-    }
-
-    /**
      * Builds a list of all post for a specific category
      */
     public function BuildPostList()
