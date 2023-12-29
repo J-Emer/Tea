dd29bf06fe900eeecc3fdafd2080e36ab9689d75
PartumGames <jemerson6989@gmail.com>
2023-12-28 18:20:41 -0600

    refactored how lists are generated
    Created the ListBuilder class. It now generates all of the lists for each category. As of now calling php Tea.php will run the ListBuilder

--- Tea.php
+++ Tea.php
@@ -1,62 +1,15 @@
 <?php
 
+use Jemer\Tea\ListBuilder;
 use Jemer\Tea\PathHelper;
 use Symfony\Component\Yaml\Yaml;
 
 require "vendor/autoload.php";
 
-$path = __DIR__ . DIRECTORY_SEPARATOR . "\Content\Posts";
-
-$categories = PathHelper::GetDirectories($path);
-
-foreach ($categories as $category) 
-{
-    $dirPath = $path . DIRECTORY_SEPARATOR . $category;
-
-    $files = RemoveList(PathHelper::GetFiles($dirPath));
-
-    $files = RemoveExtension($files);
-
-    //var_dump($files);
-
-    $data = BuildYamlArray($category, $files);
-
-    $savePath = $dirPath . DIRECTORY_SEPARATOR . "list.yaml";
-
-    file_put_contents($savePath, $data);
-}
+$listBuilder = new ListBuilder();
+$listBuilder->BuildPostList();
 
 
-
-function BuildYamlArray(string $category, array $posts)
-{
-    $array = [
-        "title" => "list",
-        "template" => "list",
-        "category" => $category,
-        "posts" => $posts,
-    ];
-
-    return Yaml::dump($array);
-}
-function RemoveList(array $files) : array
-{
-    $index = array_search('list.yaml', $files);
-    unset($files[$index]);
-    return $files;
-}
-function RemoveExtension(array $files) : array
-{
-    $data = [];
-
-    foreach ($files as $file) 
-    {
-        $name = PathHelper::NamewithoutExtension($file);
-        array_push($data, $name);
-    }
-
-    return $data;
-}
 
 
 
--- src/ListBuilder.php
+++ src/ListBuilder.php
@@ -2,8 +2,20 @@
 
 namespace Jemer\Tea;
 
+use Jemer\Tea\PathHelper;
+use Symfony\Component\Yaml\Yaml;
+
+
 class ListBuilder
 {
+
+    private $postPath;
+
+    public function  __construct()
+    {
+        $this->postPath = dirname(__DIR__, 1) . DIRECTORY_SEPARATOR . "Content\Posts";
+    }
+
     /**
      * Builds a list of categories
      */
@@ -11,13 +23,63 @@ class ListBuilder
     {
 
     }
+
     /**
      * Builds a list of all post for a specific category
      */
     public function BuildPostList()
     {
+        $categories = PathHelper::GetDirectories($this->postPath);
 
+        foreach ($categories as $category) 
+        {
+            $dirPath = $this->postPath . DIRECTORY_SEPARATOR . $category;
+        
+            $files = $this->RemoveList(PathHelper::GetFiles($dirPath));
+        
+            $files = $this->RemoveExtension($files);
+        
+            //var_dump($files);
+        
+            $data = $this->BuildYamlArray($category, $files);
+        
+            $savePath = $dirPath . DIRECTORY_SEPARATOR . "list.yaml";
+        
+            file_put_contents($savePath, $data);
+        }        
     }
+
+    private function BuildYamlArray(string $category, array $posts)
+    {
+        $array = [
+            "title" => "list",
+            "template" => "list",
+            "category" => $category,
+            "posts" => $posts,
+        ];
+    
+        return Yaml::dump($array);
+    }
+
+    private function RemoveList(array $files) : array
+    {
+        $index = array_search('list.yaml', $files);
+        unset($files[$index]);
+        return $files;
+    }
+    
+    private function RemoveExtension(array $files) : array
+    {
+        $data = [];
+    
+        foreach ($files as $file) 
+        {
+            $name = PathHelper::NamewithoutExtension($file);
+            array_push($data, $name);
+        }
+    
+        return $data;
+    }    
 }
 
 ?>
