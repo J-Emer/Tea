1b14382ff82e47e2150a032c800fb9701d55cd47
PartumGames <jemerson6989@gmail.com>
2023-12-28 19:19:14 -0600

    Update main.blade.php
    update title for site

--- Theme/Default/main.blade.php
+++ Theme/Default/main.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Tea</title>
+    <title>{{ $site->sitename}}</title>
     <link rel="stylesheet" href="@Css(bootstrap.min.css)">
 </head>
 <body>
